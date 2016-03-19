0f4ee86f30849858a9805eb756a780b774ffd712
Kevin Blanco Zuñiga <kevinbz91@gmail.com>
2016-03-19 11:04:43 -0600

    Add kevinblanco to organizers

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const organizers = {
-  gaboesquivel: require('gaboesquivel'),
-  laubits: require('laubits'),
-  brolag: require('brolag')
+  gaboesquivel 	: require('gaboesquivel'),
+  laubits 		: require('laubits'),
+  brolag 		: require('brolag'),
+  kevinblanco 	: require('kevinblanco')
 }
 
 module.exports = organizers
