0661655df5a32e14c9a38601ae282e32ebf6f685
Steven Pérez <stevenperez@Stevens-MacBook-Pro.local>
2016-03-20 10:25:29 -0600

    add Steven Pérez info merge and close #5

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const organizers = {
   gaboesquivel: require('gaboesquivel'),
   laubits: require('laubits'),
-  brolag: require('brolag')
+  brolag: require('brolag'),
+  stevenperez: require('stevenperez')
 }
 
 module.exports = organizers
