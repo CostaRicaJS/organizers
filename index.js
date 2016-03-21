ab817278a94d275d47c7b6011339fe8d74739f31
Jasson Cascante <jasson.cascante@edify.cr>
2016-03-21 17:11:29 -0600

    add Yëco's info

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const organizers = {
-  gaboesquivel: require('gaboesquivel'),
-  laubits: require('laubits'),
-  brolag: require('brolag'),
-  stevenperez: require('stevenperez'),
-  kevinblanco: require('kevinblanco'),
-  laurensortiz: require('laurensortiz')
+    gaboesquivel: require('gaboesquivel'),
+    laubits: require('laubits'),
+    brolag: require('brolag'),
+    stevenperez: require('stevenperez'),
+    kevinblanco: require('kevinblanco'),
+    laurensortiz: require('laurensortiz'),
+    yeco: require('yeco')
 }
 
 module.exports = organizers
