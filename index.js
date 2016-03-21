154b9af95e7f71f0077d958ce6478b164442e329
Jasson Cascante <jasson.cascante@edify.cr>
2016-03-21 17:49:49 -0600

    corrected indentation, thanks sublime

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const organizers = {
-    gaboesquivel: require('gaboesquivel'),
-    laubits: require('laubits'),
-    brolag: require('brolag'),
-    stevenperez: require('stevenperez'),
-    kevinblanco: require('kevinblanco'),
-    laurensortiz: require('laurensortiz'),
-    yeco: require('yeco')
+  gaboesquivel: require('gaboesquivel'),
+  laubits: require('laubits'),
+  brolag: require('brolag'),
+  stevenperez: require('stevenperez'),
+  kevinblanco: require('kevinblanco'),
+  laurensortiz: require('laurensortiz'),
+  yeco: require('yeco')
 }
 
 module.exports = organizers
